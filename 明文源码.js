303f6e4f58df3c19e41700c321d2227c1a216af2
dreamskr <43511466+dreamskr@users.noreply.github.com>
2024-12-08 21:36:30 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '4236e405-9d4f-4157-ac06-e4890ba693f0';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
